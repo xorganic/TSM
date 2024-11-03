9fb2679007ef744a966e743fbcddcfc8fed64105
Xorganic.exe <66781492+xorganic@users.noreply.github.com>
2024-11-03 05:33:47 +0200

    Update temp_files.h

--- headers/temp_files.h
+++ headers/temp_files.h
@@ -1,1 +1,9 @@
+#ifndef TEMP_FILES_H
+#define TEMP_FILES_H
+
+void create_temp_payload(const char *payload_data);  // Creează un fișier temporar cu payload-ul
+void hide_temp_files();                              // Ascunde fișierele temporare
+void delete_temp_files();                            // Șterge fișierele temporare create
+
+#endif // TEMP_FILES_H
 
