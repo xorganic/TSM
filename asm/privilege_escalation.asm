894361d8c50bb4d39b1e5dd66b8e7930fa859693
Xorganic.exe <66781492+xorganic@users.noreply.github.com>
2024-11-03 05:53:17 +0200

    Update privilege_escalation.asm

--- asm/privilege_escalation.asm
+++ asm/privilege_escalation.asm
@@ -1,1 +1,30 @@
+section .text
+    global privilege_escalate
 
+; privilege_escalate
+; Funcția încearcă să obțină privilegii administrative pe procesul curent
+privilege_escalate:
+    ; Deschidem token-ul procesului curent cu permisiuni de ajustare
+    mov rax, 0x0040                  ; Syscall pentru NtOpenProcessToken
+    mov rcx, -1                      ; Handle pentru procesul curent
+    mov rdx, TOKEN_ADJUST_PRIVILEGES ; Acces pentru ajustarea privilegiilor
+    mov r8, token_handle
+    syscall                           ; Apel syscall pentru a obține token-ul
+
+    ; Configurăm structura pentru SeDebugPrivilege
+    mov rax, 0x0041                  ; Syscall pentru NtAdjustPrivilegesToken
+    mov rcx, [token_handle]          ; Handle-ul token-ului
+    mov rdx, privilege_struct        ; Structura cu SeDebugPrivilege
+    syscall                           ; Apel syscall pentru a activa privilegiul
+    ret
+
+section .bss
+    token_handle resq 1              ; Rezervă spațiu pentru handle-ul token-ului
+
+section .data
+    ; Structura pentru SeDebugPrivilege
+    privilege_struct:
+        db 0x12, 0x00, 0x00, 0x00    ; Length
+        db 0x01                      ; Privilege count
+        db 0x00, 0x00, 0x00          ; Padding
+        db 0x02                      ; Enable privilege (SE_PRIVILEGE_ENABLED)
