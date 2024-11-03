2c9165731beb588753b68dc9b411d4faf4746705
Xorganic.exe <66781492+xorganic@users.noreply.github.com>
2024-11-03 05:49:44 +0200

    Create persistance.h

---
+++ headers/persistance.h
@@ -0,0 +1,7 @@
+#ifndef PERSISTENCE_H
+#define PERSISTENCE_H
+
+void enable_persistence();      // Activează persistența pe sistem
+void disable_persistence();     // Dezactivează mecanismele de persistență
+
+#endif // PERSISTENCE_H
