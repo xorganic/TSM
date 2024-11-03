8d22129f27bb27ef1b2f9686fa677eb09f6c7aa9
Xorganic.exe <66781492+xorganic@users.noreply.github.com>
2024-11-03 05:39:00 +0200

    Update evasion.h

--- headers/evasion.h
+++ headers/evasion.h
@@ -1,1 +1,44 @@
+section .data
+    payload db 'Injected payload', 0   ; Exemplu de payload în Assembly
+
+section .text
+    global shadow_initialize
+    global shadow_inject
+    global shadow_cleanup
+
+; Funcția de inițializare a shadow injection
+shadow_initialize:
+    ; Aici s-ar putea seta registrele sau alte configurații
+    ret
+
+; Injectează payload-ul în shadow cache
+shadow_inject:
+    ; Aici se face apel la syscall-uri pentru a crea fișierul în shadow cache
+    ; Configurăm syscall-ul pentru a crea un fișier
+    mov rax, 0x55                  ; Syscall-ul pentru NtCreateFile (valabil pentru exemplu)
+    mov rcx, shadow_file_handle    ; Adresa unde se va stoca handle-ul fișierului
+    mov rdx, shadow_path           ; Calea shadow
+    mov r8, GENERIC_WRITE
+    mov r9, FILE_ATTRIBUTE_HIDDEN  ; Setează atributele pentru a-l ascunde
+    syscall                         ; Apelează syscall-ul direct
+
+    ; Scrie payload-ul în shadow cache
+    mov rax, 0x56                  ; Syscall-ul pentru NtWriteFile
+    mov rcx, [shadow_file_handle]
+    mov rdx, payload
+    mov r8, [payload_size]         ; Mărimea payload-ului
+    syscall                         ; Apelează syscall-ul pentru a scrie payload-ul
+    ret
+
+; Curăță shadow cache-ul eliminând payload-ul
+shadow_cleanup:
+    mov rax, 0x57                  ; Syscall-ul pentru NtDeleteFile
+    mov rdx, shadow_path           ; Calea fișierului shadow
+    syscall                         ; Apelează syscall-ul pentru ștergere
+    ret
+
+section .bss
+    shadow_file_handle resq 1       ; Stocare pentru handle-ul fișierului shadow
+    payload_size equ $ - payload    ; Dimensiunea payload-ului
+    shadow_path db "C:\\Windows\\Temp\\shadow_payload.dat", 0
 
